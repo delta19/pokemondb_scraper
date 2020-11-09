cba0b3af9b39467798f6a1417b6b49958b7d6f8d
delta19 <cupbord3@gmail.com>
2020-11-08 19:26:36 -0600

    able to parse multiple URLs and write to a file

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
+import codecs
+
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://pokemondb.net/pokedex/game/sword-shield/crown-tundra"
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, "html.parser")
+OUT_FILE = "galar-dex.txt"
 
-# Getting the table entries
-entries = soup.find_all("span", class_="infocard-lg-data text-muted")
-
-# Getting just the name
-for entry in entries:
-    pokemon_name = entry.find("a", class_="ent-name")
-    print(pokemon_name.text)
+# Opening a file to write to
+with codecs.open(OUT_FILE, "w", "utf-8") as file:
+    urls = [
+        "https://pokemondb.net/pokedex/game/sword-shield",
+        "https://pokemondb.net/pokedex/game/sword-shield/isle-of-armor",
+        "https://pokemondb.net/pokedex/game/sword-shield/crown-tundra"
+    ]
+    
+    # A set to track names and make sure no duplicates are printed
+    name_set = set()
+    
+    for url in urls:
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, "html.parser")
+        
+        # Getting the table entries
+        entries = soup.find_all("span", class_="infocard-lg-data text-muted")
+        
+        # Getting just the name
+        for entry in entries:
+            pokemon_name = entry.find("a", class_="ent-name").text
+            if pokemon_name not in name_set:
+                name_set.add(pokemon_name)
+                file.write(pokemon_name + "\n")
